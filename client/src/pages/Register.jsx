4c7c0b213a7d2db2395b8ca23dbdfc5bf05afab9
Ayman-BEDDA <bedda.ayman.sio@gmail.com>
2023-12-04 12:04:35 +0100

    Fix login with token and refresh Token, logout and init dashboard

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,231 +1,139 @@
 import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import {RequiredField} from "../components/form/RequiredField.jsx";
-import Link from "@mui/material/Link";
-
-function validatePassword(password) {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    const minLength = 8;
-
-    if (!regex.test(password) || password.length < minLength ) {
-        return "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre, un caractère spécial et doit avoir au moins 8 caractères.";
-    }
-
-    if (password.length < minLength) {
-        return `Le mot de passe doit avoir au moins ${minLength} caractères.`;
-    }
-
-    return null;
-}
+import {
+    TextField,
+    Button,
+    Grid,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem, Box, Link, Input
+} from '@mui/material';
+import sendRequest from "../services/axiosRequestFunction";
+import {useNavigate} from "react-router-dom";
 
 const Register = () => {
-    const initialState = {
-        email: '',
-        password: '',
-        confirmPassword: '',
-        firstname: '',
-        lastname: '',
-        phone: '',
-        showPassword: false,
-    };
-
-    const [formData, setFormData] = useState(initialState);
-
-    const [errors, setErrors] = useState({
-        password: '',
-        confirmPassword: '',
-        phone: '',
-    });
-
-    const [successMessage, setSuccessMessage] = useState('');
-
-
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleTogglePasswordVisibility = () => {
-        setFormData({
-            ...formData,
-            showPassword: !formData.showPassword,
-        });
-    };
-
-    const validateForm = () => {
-        let isValid = true;
-        const newErrors = { ...errors };
-
-        const passwordError = validatePassword(formData.password);
-        if (passwordError) {
-            newErrors.password = passwordError;
-            isValid = false;
-        } else {
-            newErrors.password = '';
-        }
-
-        if (formData.password !== formData.confirmPassword) {
-            newErrors.confirmPassword = 'Les mots de passe ne correspondent pas.';
-            isValid = false;
-        } else {
-            newErrors.confirmPassword = '';
-        }
-
-        setErrors(newErrors);
-        return isValid;
-    };
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [role, setRole] = useState('');
+    const [kbis, setKbis] = useState('');
+    const [showKbis, setShowKbis] = useState(false);
+    const [phone, setPhone] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        sendRequest(
+            '/api/users',
+            'post',
+            {
+                email: email,
+                plainPassword: password,
+                firstname: firstname,
+                lastname: lastname,
+                roles: role === 'professionnel' ? ['ROLE_PRO'] : ['ROLE_USER'],
+                phone: phone,
+                password: password,
+            },
+            false
+        ).then(response =>
+            navigate("/login"))
+            .catch(error => console.error("Erreur lors de l'inscription:", error));
+    };
 
-        if (validateForm()) {
-            try {
-                const response = await axios.post('http://localhost:8000/api/register', {
-                    ...formData,
-                    token: 'ok',
-                });
-
-                setFormData(initialState);
-                setSuccessMessage('Votre compte a été créé, vous allez recevoir un email de validation.');
-            } catch (error) {
-                console.error("Une erreur s'est produite : ", error);
-            }
+    const handleRoleChange = (e) => {
+        setRole(e.target.value);
+        if (e.target.value === 'professionnel') {
+            setShowKbis(true);
+        } else {
+            setShowKbis(false);
+            setKbis('');
         }
     };
 
     return (
-        <Container component="main" maxWidth="xs">
-            <Paper elevation={3} style={{ padding: 20, marginTop: 20 }}>
-                <Typography variant="h5" component="div" align="center">
-                    Créer votre compte!
-                </Typography>
-                {successMessage && (
-                    <Typography variant="body1" color="primary" align="center" gutterBottom>
-                        {successMessage}
-                    </Typography>
-                )}
+        <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={12} sm={6}>
+                <Box mt={2} textAlign="center">
+                    <h2>Inscrivez-vous !</h2>
+                </Box>
                 <form onSubmit={handleSubmit}>
-                    <Box mt={2}>
-                        <TextField
-                            label="Email"
-                            variant="outlined"
-                            fullWidth
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            required
-                        />
-                    </Box>
-                    <Box mt={2}>
-                        <TextField
-                            label="Password"
-                            variant="outlined"
-                            type={formData.showPassword ? 'text' : 'password'}
-                            fullWidth
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            error={Boolean(errors.password)}
-                            helperText={errors.password}
-                            required
-                            InputProps={{
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            onClick={handleTogglePasswordVisibility}
-                                            edge="end"
-                                        >
-                                            {formData.showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Box>
-                    <Box mt={2}>
-                        <TextField
-                            label="Confirm Password"
-                            variant="outlined"
-                            type={formData.showPassword ? 'text' : 'password'}
-                            fullWidth
-                            name="confirmPassword"
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
-                            error={Boolean(errors.confirmPassword)}
-                            helperText={errors.confirmPassword}
-                            required
-                            InputProps={{
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            onClick={handleTogglePasswordVisibility}
-                                            edge="end"
-                                        >
-                                            {formData.showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Box>
-                    <Box mt={2}>
-                        <TextField
-                            label="Firstname"
-                            variant="outlined"
-                            fullWidth
-                            name="firstname"
-                            value={formData.firstname}
-                            onChange={handleChange}
-                            required
-                        />
-                    </Box>
-                    <Box mt={2}>
-                        <TextField
-                            label="Lastname"
-                            variant="outlined"
-                            fullWidth
-                            name="lastname"
-                            value={formData.lastname}
-                            onChange={handleChange}
-                            required
-                        />
-                    </Box>
-                    <Box mt={2}>
-                        <TextField
-                            label="Phone number"
-                            variant="outlined"
-                            fullWidth
-                            name="phone"
-                            value={formData.phone}
-                            onChange={handleChange}
-                            required
-                        />
-                    </Box>
-                    <Box mt={2}>
-                        <RequiredField />
-                    </Box>
-                    <Box mt={2} mb={2}>
-                        <Button type="submit" variant="contained" color="primary" fullWidth>
-                            Register
-                        </Button>
-                    </Box>
+                    <TextField
+                        label="Nom"
+                        value={firstname}
+                        onChange={(e) => setFirstname(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        label="Prénom"
+                        value={lastname}
+                        onChange={(e) => setLastname(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        label="Email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        label="Mot de passe"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        label="Téléphone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <FormControl fullWidth margin="normal">
+                        <InputLabel id="role-label">Rôle</InputLabel>
+                        <Select
+                            labelId="role-label"
+                            value={role}
+                            onChange={handleRoleChange}
+                        >
+                            <MenuItem value="particulier">Particulier</MenuItem>
+                            <MenuItem value="professionnel">Professionnel</MenuItem>
+                        </Select>
+                    </FormControl>
+                    {showKbis && (
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel htmlFor="kbis-file">KBIS</InputLabel>
+                            <Input
+                                id="kbis-file"
+                                type="file"
+                                onChange={(e) => setKbisFile(e.target.files[0])}
+                                fullWidth
+                            />
+                        </FormControl>
+                    )}
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        size="large"
+                        style={{ marginTop: '1rem' }}
+                    >
+                        S'inscrire
+                    </Button>
                 </form>
-            </Paper>
-            <Box mt={2} textAlign="center">
-                <Link href="/login">Vous avez déjà un compte? Connectez-vous!</Link>
-            </Box>
-        </Container>
+                <Box mt={2} textAlign="center">
+                    <Link href="/login">Vous n'avez pas de compte? Inscrivez-vous!</Link>
+                </Box>
+            </Grid>
+        </Grid>
     );
 };
 
