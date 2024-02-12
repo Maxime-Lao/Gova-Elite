bcb8cc0e6d58906323e319cca985807421815462
muthuxv <muthulan.m@gmail.com>
2024-02-12 12:18:08 +0100

    to do mailer

---
+++ server/src/EventSubscriber/UserRegisterSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\EventSubscriber;
+
+use ApiPlatform\Symfony\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+
+class UserRegisterSubscriber implements EventSubscriberInterface
+{
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['sendMail', EventPriorities::POST_WRITE],
+        ];
+    }
+
+    public function sendMail(ViewEvent $event): void
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $message = (new Email())
+            ->from('system@example.com')
+            ->to('muthulan.m@gmail.com')
+            ->subject('A new book has been added')
+            ->text(sprintf('The book #%d has been added.'));
+
+        $this->mailer->send($message);
+    }
+}
