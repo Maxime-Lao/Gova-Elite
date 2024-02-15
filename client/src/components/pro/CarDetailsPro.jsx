e5d01a7d2f0f4c3cc09f91d356c8a12586a75486
muthuxv <muthulan.m@gmail.com>
2024-02-15 18:48:55 +0100

    add pro stats

--- client/src/components/pro/CarDetailsPro.jsx
+++ client/src/components/pro/CarDetailsPro.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import CarSchedule from "./CarSchedule.jsx";
 import DatePicker from 'react-datepicker';
--- client/src/components/pro/CompanieDetails.jsx
+++ client/src/components/pro/CompanieDetails.jsx
@@ -1,19 +1,140 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Grid, Box, Card, CardContent, Typography } from '@mui/material';
+import CarRentalTwoToneIcon from '@mui/icons-material/CarRentalTwoTone';
+import { Bar, Pie } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 const CompanieDetails = ({ companie }) => {
     const { name, address, city, zipCode, cars } = companie;
+    const [rents, setRents] = useState([]);
+    const [monthlyIncome, setMonthlyIncome] = useState(0);
+
+
+    const fetchRentData = useCallback(async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/companies/${companie.id}/rents`);
+            const data = await response.json();
+            setRents(data);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données de location : ', error);
+        }
+    }, [companie.id]);
+
+    useEffect(() => {
+        fetchRentData();
+    }, [fetchRentData]);
+
+    const calculateMonthlyIncome = (rentsData, month, year) => {
+        let totalIncome = 0;
+        rentsData.forEach(rent => {
+            const rentDate = new Date(rent.dateStart);
+            if (rentDate.getMonth() === month && rentDate.getFullYear() === year) {
+                totalIncome += rent.totalPrice;
+            }
+        });
+        return totalIncome;
+    };
+
+    const generateChartData = useCallback(() => {
+        const currentDate = new Date();
+        const currentMonth = currentDate.getMonth();
+        const currentYear = currentDate.getFullYear();
+
+        const monthlyIncomeData = [];
+        for (let month = 0; month <= currentMonth; month++) {
+            const income = calculateMonthlyIncome(rents, month, currentYear);
+            monthlyIncomeData.push(income);
+        }
+        setMonthlyIncome(monthlyIncomeData);
+    }, [rents]);
+
+    useEffect(() => {
+        generateChartData();
+    }, [generateChartData]);
 
     return (
-        <div className="max-w-md mx-auto p-6 bg-white shadow-md mb-4">
-            <h1 className="text-2xl font-bold mb-2">{name}</h1>
-            <p className="text-gray-600 mb-1 font-bold">Address:</p>
-            <p className="mb-2">{address}</p>
-            <p className="text-gray-600 mb-1">{city}, {zipCode}</p>
-            <hr className="my-4 border-t-2 border-gray-300" />
-            <div className="mt-4">
-                <p className="mb-2 text-gray-600 font-bold">Nombre de voitures: {cars.length}</p>
-            </div>
-        </div>
+        <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" gutterBottom>
+                            Informations de la société
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <strong>Nom:</strong> {name}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <strong>Adresse:</strong> {address}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <strong>Ville:</strong> {city}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <strong>Code Postal:</strong> {zipCode}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card>
+                    <CardContent sx={{ alignItems: 'center' }}>
+                        <Typography variant="caption" gutterBottom>
+                            Nombre total de voitures
+                        </Typography>
+                        <Typography variant="h3" gutterBottom align="center">
+                            {cars.length}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card>
+                    <CardContent>
+                        <CarRentalTwoToneIcon />
+                        <Typography variant="caption" gutterBottom>
+                            Nombre total de locations
+                        </Typography>
+                        <Typography variant="h3" gutterBottom align="center">
+                            {rents.length}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Card>
+                    <CardContent>
+                        <Typography variant="h5" gutterBottom>
+                            Chiffre d'affaires mensuel
+                        </Typography>
+                        <Box sx={{ position: 'relative', height: 400 }}>
+                        <Bar
+                            data={{
+                                labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+                                datasets: [
+                                    {
+                                        label: 'Revenu mensuel',
+                                        data: monthlyIncome,
+                                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                                        borderColor: 'rgba(75, 192, 192, 1)',
+                                        borderWidth: 1,
+                                    },
+                                ],
+                            }}
+                            options={{
+                                scales: {
+                                    y: {
+                                        beginAtZero: true,
+                                    },
+                                },
+                            }}
+                        />
+                        </Box>
+                    </CardContent>
+
+                </Card>
+            </Grid>
+        </Grid>
     );
 };
 
