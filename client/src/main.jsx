cd1deebd9f1322ad4095a42d5b42f2774013d153
SimonBTSSio <simon.farnault.sio@gmail.com>
2024-02-16 08:56:56 +0000

    footer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Footer from "./components/others/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <>
+        <App />
+        <Footer />
+    </>
 )
